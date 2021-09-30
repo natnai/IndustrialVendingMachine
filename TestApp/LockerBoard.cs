59501d9d0e29075f4bfe6b47d524a6c387de92bd
natnai <91661076+natnai@users.noreply.github.com>
2021-09-30 16:33:22 +0700

    Initial release

---
+++ TestApp/LockerBoard.cs
@@ -0,0 +1,166 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO.Ports;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Test_LockerBoard
+{
+    class LockerBoard
+    {
+        SerialPort serial = null;
+
+        List<byte> byteReceived = new List<byte>();
+        string dataReceived = "";
+        DateTime lastDataReceived = DateTime.MinValue;
+        Thread thread = null;
+
+        public LockerBoard(string portName)
+        {
+            serial = new SerialPort(portName)
+            {
+                BaudRate = 9600,
+                Parity = Parity.None,
+                DataBits = 8,
+                StopBits = StopBits.One,
+                ReadTimeout = 1000,
+                WriteTimeout = 1000,
+                ReceivedBytesThreshold = 1
+            };
+            serial.DataReceived += DataReceivedHandler;
+
+            this.thread = new Thread(LogicLoop);
+            this.thread.Start();
+        }
+
+        public void OpenDoor(byte boardAddr, byte lockerAddr)
+        {
+            var buffer = new List<byte>() { 0x8A, boardAddr, lockerAddr, 0x11 };
+            buffer.Add(this.CalculateChecksum(buffer.ToArray()));
+
+            this.SendData(buffer.ToArray());
+        }
+
+        public byte[] ReadDoors(byte boardAddr)
+        {
+            //clear buffer
+            try { this.serial.ReadExisting(); } catch { } 
+
+            var buffer = new List<byte>() { 0x80, boardAddr, 0x00, 0x33 };
+            buffer.Add(this.CalculateChecksum(buffer.ToArray()));
+
+            this.SendData(buffer.ToArray());
+
+            //Task.Delay(500).Wait();
+
+            return this.ReadData();
+        }
+
+        private void SendData(byte[] buffer)
+        {
+            if (this.serial.IsOpen == false) { this.serial.Open(); }
+
+            serial.Write(buffer, 0, buffer.Length);
+        }
+
+        private byte[] ReadData()
+        {
+            /*
+            if (this.serial.IsOpen == false) { this.serial.Open(); }
+
+            bool done = false;
+            string res = "";
+            while (!done)
+            {
+                try
+                {
+                    var data = serial.ReadExisting();
+                    if (data != "")
+                    {
+                        res = res + data;
+                        Task.Delay(500).Wait();
+                    }
+                    else
+                    {
+                        done = true;
+                    }
+                }
+                catch { done = true; }
+            }
+            */
+            var res = "";
+            return Encoding.ASCII.GetBytes(res);
+        }
+
+        private void DataReceivedHandler(object sender, SerialDataReceivedEventArgs e)
+        {
+            SerialPort sp = (SerialPort)sender;
+
+            var result = new List<byte>();
+            var i = sp.ReadByte();
+            while(i >=0 )
+            {
+                result.Add(Convert.ToByte(i));
+
+                try { i = sp.ReadByte(); } catch { i = -1; }
+            }
+
+            if (result.Count > 0)
+            {
+                this.byteReceived.AddRange(result);
+                this.lastDataReceived = DateTime.Now;
+            }
+
+            /*
+            string indata = sp.ReadExisting();
+
+            if (indata != "")
+            {
+                this.dataReceived = this.dataReceived + indata;
+                this.lastDataReceived = DateTime.Now;
+            }
+            */
+        }
+
+        private void LogicLoop()
+        {
+            while (true)
+            {
+                /*
+                if (this.dataReceived != "" && (DateTime.Now - this.lastDataReceived).TotalSeconds >= 1)
+                {
+                    Console.WriteLine();
+                    Console.Write("Data Received: ");
+                    var buffer = Encoding.ASCII.GetBytes(this.dataReceived);
+                    foreach (var b in buffer) { Console.Write("{0:X2} ", b); }
+                    Console.WriteLine();
+
+                    this.dataReceived = "";
+                }
+                */
+
+                if (this.byteReceived.Count > 0 && (DateTime.Now - this.lastDataReceived).TotalSeconds >= 1)
+                {
+                    Console.WriteLine();
+                    Console.Write("Data Received: ");
+                    foreach (var b in this.byteReceived) { Console.Write("{0:X2} ", b); }
+                    Console.WriteLine();
+
+                    this.byteReceived.Clear();
+                }
+
+                Task.Delay(100).Wait();
+            }
+        }
+
+        private byte CalculateChecksum(byte[] buffer)
+        {
+            Byte result = 0x00;
+            for (int i = 0; i < buffer.Length; i++)
+                result ^= buffer[i];
+            return result;
+        }
+    }
+}
---
+++ TestApp/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test_LockerBoard
+{
+          //https://th.aliexpress.com/item/33005274059.html?spm=a2g0o.productlist.0.0.30e83f3621w5H0&algo_pvid=366d0efb-327e-4234-883f-41d6cb557bd7&algo_expid=366d0efb-327e-4234-883f-41d6cb557bd7-1&btsid=0b0a556f16218408785565470e33ce&ws_ab_test=searchweb0_0,searchweb201602_,searchweb201603_
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            byte controllerId = 1;
+
+            Console.Write("Enter COM Port [0-255]: ");
+            var portName = "COM" + Console.ReadLine();
+
+            Console.WriteLine("Openning " + portName);
+            LockerBoard locker = new LockerBoard(portName);
+
+            ConsoleKeyInfo cmd = new ConsoleKeyInfo();
+            while (cmd.Key != ConsoleKey.D0 && cmd.Key != ConsoleKey.NumPad0)
+            {
+                Console.WriteLine("");
+                Console.WriteLine("MENU");
+                Console.WriteLine("  [1] Open all doors");
+                Console.WriteLine("  [2] Open 1 door");
+                Console.WriteLine("  [3] Read status all doors");
+                Console.WriteLine("  ---------");
+                Console.WriteLine("  [0] Exit");
+
+                Console.Write("Enter command: ");
+
+                cmd = Console.ReadKey();
+                Console.WriteLine("");
+
+                try
+                {
+                    if (cmd.Key == ConsoleKey.D1 || cmd.Key == ConsoleKey.NumPad1)
+                    {
+                        //locker.OpenDoor(1, 0);
+                        //locker.OpenDoor(2, 0);
+                        //locker.OpenDoor(3, 0);
+                        locker.OpenDoor(4, 0);
+                        locker.OpenDoor(5, 0);
+                    }
+                    else if (cmd.Key == ConsoleKey.D2 || cmd.Key == ConsoleKey.NumPad2)
+                    {
+                        Console.Write("Enter locker number [1-255]: ");
+                        var num = Console.ReadLine();
+                        if (byte.TryParse(num, out byte b))
+                        {
+                            locker.OpenDoor(
+                                Convert.ToByte(Math.Ceiling((decimal)b / (decimal)12)),
+                                (byte)(b - Convert.ToByte((Math.Floor((decimal)b / (decimal)12)) * 12))
+                            );
+                        }
+                    }
+                    else if (cmd.Key == ConsoleKey.D3 || cmd.Key == ConsoleKey.NumPad3)
+                    {
+                        var bytes = locker.ReadDoors(controllerId);
+                        //Console.Write("Response: ");
+                        foreach (var b in bytes) { Console.Write("{0:X2} ", b); }
+
+                        Console.WriteLine();
+                    }
+                }
+                catch( Exception ex)
+                {
+                    Console.WriteLine();
+                    Console.WriteLine("EXCEPTION: " + ex.Message);
+                    Console.WriteLine();
+                }
+            }
+        }
+    }
+}
